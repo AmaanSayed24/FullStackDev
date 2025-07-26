32b9543dd04030884f1dd7b3f47abc4fd987ffb5
AmaanSayed24 <amaansayed2004@gmail.com>
2025-07-26 12:05:39 +0530

    commit

---
+++ Lab5/filesforlab5/stream.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const filesteam = fs.createReadStream('log.txt','utf-8');
+
+let readfiledata = '';
+
+filesteam.on('data', (data) => {
+  readfiledata += data;
+});
+
+filesteam.on('end', (data) => {
+  console.log('File read completed');
+})
+
+filesteam.on('error', () => {
+  console.error('I LIKE BOOBS');
+});
+
+filesteam.on('open', () => {
+  console.log('File opened successfully');
+})
+
+filesteam.on('close', () => {
+  console.log('File closed successfully');
+})
